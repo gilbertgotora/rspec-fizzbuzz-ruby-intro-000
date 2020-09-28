eac249ccb2884906aa29737fd4b4e62bdb2bfb32
Gilbert Gotora <gilbertfgotora@gmail.com>
2020-09-28 20:53:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,4 @@
-require_relative './spec_helper.rb'
+# require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
